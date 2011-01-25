f81dd088f490dc20422a5778332f7446412991f9
loxK <lox.dev@knc.nc>
2011-01-25 15:17:28 +1100

    Working libsensors. TODO: remove proximity sensor
    New libsensors source using new sensors HAL
    
    Include libsensors makefile

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -12,31 +12,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),hero)
 
-LOCAL_PATH := $(call my-dir)
-
-# HAL module implemenation, not prelinked and stored in
+# HAL module implemenation, not prelinked, and stored in
 # hw/<SENSORS_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
-LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils
-LOCAL_SRC_FILES := sensors.c
+
 LOCAL_MODULE := sensors.hero
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_CFLAGS := -DLOG_TAG=\"Sensors\"
+LOCAL_SRC_FILES := 						\
+				sensors.c 				\
+				nusensors.cpp 			\
+				InputEventReader.cpp	\
+				SensorBase.cpp			\
+				LightSensor.cpp			\
+				ProximitySensor.cpp		\
+				AkmSensor.cpp
+				
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_PRELINK_MODULE := false
+
 include $(BUILD_SHARED_LIBRARY)
 
-# HAL module implemenation, not prelinked and stored in
-# hw/<SENSORS_HARDWARE_MODULE_ID>.<ro.product.board>.so
-#include $(CLEAR_VARS)
-#LOCAL_PRELINK_MODULE := false
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-#LOCAL_SHARED_LIBRARIES := liblog libcutils
-#LOCAL_SRC_FILES := sensors.c
-#LOCAL_MODULE := sensors.trout
-#include $(BUILD_SHARED_LIBRARY)
-
-endif
+endif # TARGET_BOOTLOADER_BOARD_NAME
