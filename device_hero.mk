6ae3bd08dbe0eb7bfe38aa3d2ac3c4372c97b46e
Esa Laukkanen <ele.linux@gmail.com>
2011-04-29 22:53:32 +0200

    New massupdate,updated kernel,add tun.ko etc....
    Change-Id: Ib008fa75d411fd29721592df010185f201e13db1

--- device_hero.mk
+++ device_hero.mk
@@ -29,6 +29,11 @@ PRODUCT_COPY_FILES += \
 # proprietary side of the device
 $(call inherit-product-if-exists, vendor/htc/hero/device_hero-vendor.mk)
 
+# No zram in kernel, so use ramzswap for compcache
+PRODUCT_COPY_FILES += \
+    device/htc/hero/prebuilt/12compcache:system/etc/init.d/12compcache \
+    device/htc/hero/prebuilt/rzscontrol:system/xbin/rzscontrol
+
 PRODUCT_PACKAGES += \
     librs_jni \
     hero-keypad.kcm \
@@ -155,6 +160,7 @@ PRODUCT_COPY_FILES += \
     device/htc/hero/modules/modules.dep.bb:system/lib/modules/$(KERNEL_NAME)/modules.dep.bb \
     device/htc/hero/modules/modules.order:system/lib/modules/$(KERNEL_NAME)/modules.order \
     device/htc/hero/modules/ip_gre.ko:system/lib/modules/$(KERNEL_NAME)/net/ipv4/ip_gre.ko \
+    device/htc/hero/modules/tun.ko:system/lib/modules/$(KERNEL_NAME)/net/tun.ko \
     device/htc/hero/modules/wlan.ko:system/lib/modules/$(KERNEL_NAME)/drivers/net/wireless/tiwlan1251/wlan.ko \
     device/htc/hero/modules/hid-dummy.ko:system/lib/modules/$(KERNEL_NAME)/drivers/hid/hid-dummy.ko \
     device/htc/hero/modules/ramzswap.ko:system/lib/modules/$(KERNEL_NAME)/drivers/staging/ramzswap/ramzswap.ko \
